a54d7f9486e0ccc5ba67a76824f7bed38af6d639
Rush <itzashert@gmail.com>
2024-10-12 00:39:46 +0530

    Calling him

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import logo from '../assets/logo.webp'
 
 function Navbar() {
   return (
     <>
-    <div className='w-full h-10 bg-black'>
-
+    <div className='w-full h-[50px] bg-black flex flex-row justify-between align-center px-32'>
+      <div className='logo h-10 w-10'> <img src={logo} alt="" /></div>
+      
     </div>
     </>
   )
